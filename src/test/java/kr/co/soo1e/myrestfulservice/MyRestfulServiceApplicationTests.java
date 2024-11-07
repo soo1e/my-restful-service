6ab3282790195d7292079dc0ad09e598f32a4316
soo1e <gameswuon@gmail.com>
2024-11-08 00:30:08 +0900

    Initial commit

---
+++ src/test/java/kr/co/soo1e/myrestfulservice/MyRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.soo1e.myrestfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
